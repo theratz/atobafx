1430e3da5ab5e765a814c2fd0113cc7c829fb635
theratz <49798928+theratz@users.noreply.github.com>
2022-01-28 10:58:45 +0630

    Update install.sh

--- warp/install.sh
+++ warp/install.sh
@@ -3,5 +3,5 @@ apt-get update && apt-get upgrade -y
 apt-get install python wget -y
 pip install --upgrade pip
 
-wget https://raw.githubusercontent.com/theratz/atobafx/main/warp/warp -O ../usr/bin/warp
+cd && wget https://raw.githubusercontent.com/theratz/atobafx/main/warp/warp -O ../usr/bin/warp
 chmod 777 ../usr/bin/warp
